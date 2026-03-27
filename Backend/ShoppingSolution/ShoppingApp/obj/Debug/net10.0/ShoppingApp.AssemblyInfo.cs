8202b539d2ba1b2acb96755b55f2db289a681526
Padma-Kumar-NAF <PadmaKumar.C@Nafinc.com>
2026-03-27 09:04:37 +0530

    Xunit created

--- Backend/ShoppingSolution/ShoppingApp/obj/Debug/net10.0/ShoppingApp.AssemblyInfo.cs
+++ Backend/ShoppingSolution/ShoppingApp/obj/Debug/net10.0/ShoppingApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShoppingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd8c0dc7244944d50e828bd02bda92e16919fd1c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34f3299dea2bcb4c9081172def9aa33d1cf57540")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShoppingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShoppingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
